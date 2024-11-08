8f6ceb30d27a0a2905b5a765ea274cb9a6bebc0b
Celine Kazumi Miyajima <celine.miyajima@unifesp.br>
2024-11-07 23:37:54 -0300

    Delete exemplogravação.c

--- exemplogravação.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(){
-  FILE*arq; //cria variável ponteiro para o arquivo
-char palavra[20]; // variável tipo string
-
-//abrindo arquivo com o tipo de abertura w
-arq = fopen("arquivopalavra.txt

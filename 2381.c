c933b898778cf1c30f50bb0e0eea10f1a3a391db
Celine Kazumi Miyajima <celine.miyajima@unifesp.br>
2024-11-24 14:24:57 -0300

    Update 2381.c

--- 2381.c
+++ 2381.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
-int compare(const void *a, const void *b) {
-    return strcmp(*(const char **)a, *(const char **)b);
+void Intercala(int p, int q, int r, char v[][21], char temp[][21]) {
+    int i = p, j = q, k = 0;
+    while (i < q && j < r) {
+        if (strcmp(v[i], v[j]) <= 0) {
+            strcpy(temp[k++], v[i++]);
+        } else {
+            strcpy(temp[k++], v[j++]);
+        }
+    }
+    while (i < q) {
+        strcpy(temp[k++], v[i++]);
+    }
+    while (j < r) {
+        strcpy(temp[k++], v[j++]);
+    }
+    for (i = 0; i < k; i++) {
+        strcpy(v[p + i], temp[i]);
+    }
+}
+
+void Mergesort(int p, int r, char v[][21], char temp[][21]) {
+    if (p < r - 1) {
+        int q = (p + r) / 2;
+        Mergesort(p, q, v, temp);
+        Mergesort(q, r, v, temp);
+        Intercala(p, q, r, v, temp);
+    }
 }
 
 int main() {
-    int N, K;
-    scanf("%d %d", &N, &K);
-    char alunos[N][21];
-    for (int i = 0; i < N; i++) {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    char alunos[x][21], temp[x][21];
+    for (int i = 0; i < x; i++) {
         scanf("%s", alunos[i]);
     }
-    qsort(alunos, N, sizeof(alunos[0]), compare);
-    printf("%s\n", alunos[K - 1]);
-    srand(time(NULL));
-    int S = (rand() % N) + 1;
-    printf("\n%d\n", S);
-    printf("%s\n", alunos[S - 1]);
+    Mergesort(0, x, alunos, temp);
+    printf("%s\n", alunos[y - 1]);
     return 0;
 }

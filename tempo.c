2bb32bf834185702f289d6c4f1f9c73d567cf78d
Celine Kazumi Miyajima <celine.miyajima@unifesp.br>
2024-12-03 21:08:45 -0300

    Create tempo.c

---
+++ tempo.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //clock(), CLOCKS_PER_SEC e clock_t
+
+int const TAM = 20000; //constante para tamanho do vetor
+
+void bubbleSort (int v[TAM]) {
+    int a, b, aux;
+    for (a=TAM-1; a>=1; a--) {
+        for (b=0; b<a; b++) {
+            if (v[b]>v[b+1]) {
+                aux = v[b];
+                v[b] = v[b+1];
+                v[b+1] = aux;
+            }
+        }
+    }
+}
+
+int main(){
+    clock_t t; //variável para armazenar tempo
+    int vetor[TAM];
+    
+    //semente de aleatoriedade
+    srand((unsigned)time(NULL));
+
+    //geração aleatório dos valores do vetor
+    for(int a = 0; a < TAM; a++)
+        vetor[a] = rand() % TAM;
+
+    //Verificando tempo de execução do bubble sort=> t2
+    t = clock(); //armazena tempo
+    bubbleSort(vetor);
+    t = clock() - t; //tempo final - tempo 
+
+    //imprime o tempo na tela
+    printf("Tempo de execucao: %lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000))); //conversão para double
+
+    return 0;
+}

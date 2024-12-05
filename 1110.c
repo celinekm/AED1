8dfa37edb6baa030c28a7c3075b5af6fbc5b63b6
Celine Kazumi Miyajima <celine.miyajima@unifesp.br>
2024-12-05 14:44:48 -0300

    Update 1110.c

--- 1110.c
+++ 1110.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+void processarPilha(int n) {
+    int pilha[50000], descartadas[50000];
+    int topo = 0, base = 0; 
+    int qtddescartadas = 0;
+    for (int i = 1; i <= n; i++) {
+        pilha[base++] = i;
+    }
+    while (base - topo > 1) {
+        descartadas[qtddescartadas++] = pilha[topo++]; 
+        pilha[base++] = pilha[topo++];                
+    }
+    printf("Discarded cards:");
+    for (int i = 0; i < qtddescartadas; i++) {
+        if (i == 0) {
+            printf(" %d", descartadas[i]);
+        } else {
+            printf(", %d", descartadas[i]);
+        }
+    }
+    printf("\n");
+    printf("Remaining card: %d\n", pilha[topo]);
+}
+
+int main() {
+    int n;
+    while (1) {
+        scanf("%d", &n);
+        if (n == 0) break;
+        processarPilha(n);
+    }
+
+    return 0;
+}

50e3a74ac5030d792bf93beae11d0753cea087a5
Celine Kazumi Miyajima <celine.miyajima@unifesp.br>
2024-12-08 11:05:58 -0300

    Create 1160.c

---
+++ 1160.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int T;  
+    scanf("%d", &T);
+    while (T--) {
+        int PA, PB;
+        float G1, G2;
+        scanf("%d %d %f %f", &PA, &PB, &G1, &G2);   
+        int anos = 0;
+        while (PA <= PB) {
+            PA += (int)(PA * G1 / 100);  
+            PB += (int)(PB * G2 / 100);  
+            anos++;
+            if (anos > 100) {
+                printf("Mais de 1 seculo.\n");
+                break;
+            }
+        }
+        if (anos <= 100) {
+            printf("%d anos.\n", anos);
+        }
+    }
+    return 0;
+}

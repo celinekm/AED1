68681a6d8cf88d8bfbc5994eb1691dbbf8fc30ba
Celine Kazumi Miyajima <celine.miyajima@unifesp.br>
2024-12-10 22:04:02 -0300

    Update implementaçãoquicksort.c

--- implementaçãoquicksort.c
+++ implementaçãoquicksort.c
@@ -26,14 +26,19 @@ void quickSort(int v[], int low, int high) {
 }
 
 int main() {
-    clock_t t; 
-    int vetor[MAX];
-    srand((unsigned)time(NULL));
-    for (int a = 0; a < MAX; a++)
-        vetor[a] = rand() % MAX;
-    t = clock(); 
-    quickSort(vetor, 0, MAX - 1);
-    t = clock() - t; 
-    printf("Tempo de execucao: %lf ms\n", ((double)t) / ((CLOCKS_PER_SEC / 1000))); 
+    int V[MAX];
+    for (int k = 0; k<3; k++){
+        printf("repeticao %d \n\n", k+1);
+        for (int n = 0; n <= MAX; n += 20000) {
+        for (int i = 0; i < n; i++) {
+            V[i] = rand() % 1000000;
+        }
+        clock_t comeco = clock();
+        mergeSort(V, 0, n - 1);
+        clock_t fim = clock();
+        double demorou = ((double)(fim - comeco)) / CLOCKS_PER_SEC;
+        printf("N = %d, Tempo = %f segundos\n", n, demorou);
+        }
+    }
     return 0;
 }

ec904f9f9dc81cdb3b5b714c338aea24087b3b8d
Celine Kazumi Miyajima <celine.miyajima@unifesp.br>
2024-12-10 22:07:46 -0300

    Update implementaçãomergesort.c

--- implementaçãomergesort.c
+++ implementaçãomergesort.c
@@ -57,20 +57,18 @@ void mergeSort(int V[], int esquerda, int direita) {
 
 int main() {
     int V[MAX];
-
-    for (int n = 0; n <= MAX; n += 50000) {
+    for (int k = 0; k<3; k++){
+        printf("repeticao %d \n\n", k+1);
+        for (int n = 0; n <= MAX; n += 20000) {
         for (int i = 0; i < n; i++) {
             V[i] = rand() % 1000000;
         }
-
         clock_t comeco = clock();
         mergeSort(V, 0, n - 1);
         clock_t fim = clock();
-
         double demorou = ((double)(fim - comeco)) / CLOCKS_PER_SEC;
-
         printf("N = %d, Tempo = %f segundos\n", n, demorou);
+        }
     }
-
     return 0;
 }

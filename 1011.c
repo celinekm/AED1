1e6c6b72918dbe977ec4f95f1069415a6e26f07a
Celine Kazumi Miyajima <celine.miyajima@unifesp.br>
2025-01-20 21:37:17 -0300

    Delete 1011.c

--- 1011.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    double R;  
-    scanf("%lf", &R);  
-    double volume = (4.0 / 3) * 3.14159 * (R * R * R);
-    printf("VOLUME = %.3lf\n", volume);
-    return 0;
-}

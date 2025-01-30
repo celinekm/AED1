bae944efc02797bb8e22b110dd4571a6d12a7e68
Celine Kazumi Miyajima <celine.miyajima@unifesp.br>
2025-01-30 15:47:11 -0300

    Create 1011.c

---
+++ 1011.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    double R;  
+    scanf("%lf", &R);  
+    double volume = (4.0 / 3) * 3.14159 * (R * R * R);
+    printf("VOLUME = %.3lf\n", volume);
+
+    return 0;
+}
